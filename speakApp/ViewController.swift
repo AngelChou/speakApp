224ae52b7222ca7010935fa69d16d9c73b169b3a
Angel Chou <cat77120377@hotmail.com>
2019-04-18 20:40:46 +0800

    practice AVSpeechUtterance

--- speakApp/ViewController.swift
+++ speakApp/ViewController.swift
@@ -12,19 +12,44 @@ import AVFoundation
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var voiceSegment: UISegmentedControl!
+    @IBOutlet var sliders: [UISlider]!
+    @IBOutlet var labels: [UILabel]!
+    
+    var voices = ["en-US", "zh-TW"]
+    var animalVoices = ["squeak", "moo", "meow", "hiss", "neigh", "baa", "woof", "oink", "roar", "quack", "tweet", "cluck", "ribbit", "growl", "pawoo"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for (i, slider) in sliders.enumerated() {
+            labels[i].text = String(format: "%.2f", slider.value)
+        }
     }
-
-    @IBAction func buttonClicked(_ sender: Any) {
-
-        let speechUtterance =  AVSpeechUtterance(string: messageTextField.text!)
-//        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+    
+    func speak(str: String, lan: String) {
+        let speechUtterance =  AVSpeechUtterance(string: str)
+        speechUtterance.rate = sliders[0].value
+        speechUtterance.pitchMultiplier = sliders[1].value
+        speechUtterance.volume = sliders[2].value
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: lan)
         let synthesizer = AVSpeechSynthesizer()
         synthesizer.speak(speechUtterance)
     }
     
+    @IBAction func buttonClicked(_ sender: Any) {
+        if let text = messageTextField.text {
+            speak(str: text, lan: voices[voiceSegment.selectedSegmentIndex])
+        }
+    }
+    
+    
+    @IBAction func speakButtonClicked(_ sender: UIButton) {
+        speak(str: animalVoices[sender.tag], lan: voices[0])
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let index = sender.tag
+        labels[index].text = String(format: "%.2f", sliders[index].value)
+    }
 }
 
